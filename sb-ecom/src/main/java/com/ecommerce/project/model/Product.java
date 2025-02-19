27e1503b89069acdd6793b2c4e33b29982808356
Ramsai999 <ramsapuri@gmail.com>
2025-02-19 22:32:04 +0530

    Implemented CartService with CRUD operations

--- sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -8,6 +8,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -40,5 +44,7 @@ public class Product {
     @JoinColumn(name = "seller_id")
     private User user;
 
+    @OneToMany(mappedBy = "product", cascade = { CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products = new ArrayList<>();
 
 }
